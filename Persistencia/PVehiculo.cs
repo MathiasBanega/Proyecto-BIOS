ce335a93ad2ccb91c9410ced0726c2ee2b1e3209
Mathías Banega <40126086+MathiasBanega@users.noreply.github.com>
2018-06-14 21:45:36 -0300

    Update PVehiculo.cs

--- Persistencia/PVehiculo.cs
+++ Persistencia/PVehiculo.cs
@@ -17,21 +17,21 @@ namespace Persistencia
            SqlCommand _comando = new SqlCommand("EliminarVehiculo ",_conexion);
            _comando.CommandType = CommandType.StoredProcedure;
 
-           //se le dan los parametros para el PA
+           
            SqlParameter _matricula = new SqlParameter("@mat",_Vehiculo.Matricula);
-           //variable de retorno
+           
            SqlParameter _retorno = new SqlParameter("@retorno", SqlDbType.Int);
            _retorno.Direction = ParameterDirection.ReturnValue;
-           //se cargan los valores al comando.
+          
            _comando.Parameters.Add(_matricula);
            _comando.Parameters.Add(_retorno);
 
            try
            {
-               _conexion.Open();//abre conexion
-               _comando.ExecuteNonQuery();//ejecuta el comando con todo cargado
+               _conexion.Open();
+               _comando.ExecuteNonQuery();
 
-               //se tiran los mensajes correspondiente segun el valor de la variable de retorno
+              
                if ((int)_retorno.Value == -1)
                    throw new Exception("El Vehiculo no existe.");
                else if ((int)_retorno.Value == -2)
